7ecd57870a3159c90280dd9a7494579a72d8aec5
KartikAnandBhandari <kartikbhnndari@gmail.com>
2025-03-14 01:08:39 +0530

    NETFLIX CLONE

---
+++ src/config.js
@@ -0,0 +1,20 @@
+// replace below firebase config with your own
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBg5kelqPycUFO1w3nT0ql15QXm2zoVb64",
+  authDomain: "netflix-clone-e49b2.firebaseapp.com",
+  projectId: "netflix-clone-e49b2",
+  storageBucket: "netflix-clone-e49b2.firebasestorage.app",
+  messagingSenderId: "234540394179",
+  appId: "1:234540394179:web:2c57f907bebb4e48116476"
+};
+
+
+
+// craete a/c on themoviedb.org and replace access token below
+
+
+const TMDB_Access_Key="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMzBkMjQ5ODYxY2VkNTY1NGE3MjQzZDUwMThmZGE0ZCIsIm5iZiI6MTc0MTg5MjcxNy44MzUsInN1YiI6IjY3ZDMyYzZkZGQxMmVlMzg2MDdmNGQ0ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6jqWS0_45JOGnT9oqpAV7subNS5WPxtmWDQqtQAzmro";
+
+
+  export {firebaseConfig, TMDB_Access_Key};
